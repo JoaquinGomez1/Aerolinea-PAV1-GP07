46d752ee744d9ef8e5ec56e9259aa5bcb0fd7371
Mora De <mdebianchetti81@gmail.com>
2021-05-08 21:10:54 -0300

    abm asientos

--- TrabajoPrácticoPAV/Formularios/Asientos/Frm_ABMAsientos.Designer.cs
+++ TrabajoPrácticoPAV/Formularios/Asientos/Frm_ABMAsientos.Designer.cs
@@ -30,21 +30,26 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.grilla_ABMAsiento = new TrabajoPrácticoPAV.Clase.DataGridView_Aerolinea();
-            this.cmb_NumeroAvion = new TrabajoPrácticoPAV.Clase.ComboBox_Aerolinea();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
-            this.cmb_Modelo = new TrabajoPrácticoPAV.Clase.ComboBox_Aerolinea();
-            this.btn_buscar = new TrabajoPrácticoPAV.Clase.Button_Aerolinea();
-            this.btn_registrar = new TrabajoPrácticoPAV.Clase.Button_Aerolinea();
-            this.btn_modificar = new TrabajoPrácticoPAV.Clase.Button_Aerolinea();
-            this.btn_eliminar = new TrabajoPrácticoPAV.Clase.Button_Aerolinea();
-            this.btn_cerrar = new TrabajoPrácticoPAV.Clase.Button_Aerolinea();
-            this.btn_limpiar = new TrabajoPrácticoPAV.Clase.Button_Aerolinea();
             this.chk_asientos = new System.Windows.Forms.CheckBox();
             this.label6 = new System.Windows.Forms.Label();
             this.cmb_Clase = new TrabajoPrácticoPAV.Clase.ComboBox_Aerolinea();
+            this.btn_limpiar = new TrabajoPrácticoPAV.Clase.Button_Aerolinea();
+            this.btn_cerrar = new TrabajoPrácticoPAV.Clase.Button_Aerolinea();
+            this.btn_eliminar = new TrabajoPrácticoPAV.Clase.Button_Aerolinea();
+            this.btn_modificar = new TrabajoPrácticoPAV.Clase.Button_Aerolinea();
+            this.btn_registrar = new TrabajoPrácticoPAV.Clase.Button_Aerolinea();
+            this.btn_buscar = new TrabajoPrácticoPAV.Clase.Button_Aerolinea();
+            this.cmb_Modelo = new TrabajoPrácticoPAV.Clase.ComboBox_Aerolinea();
+            this.cmb_NumeroAvion = new TrabajoPrácticoPAV.Clase.ComboBox_Aerolinea();
+            this.grilla_ABMAsiento = new TrabajoPrácticoPAV.Clase.DataGridView_Aerolinea();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.grilla_ABMAsiento)).BeginInit();
             this.SuspendLayout();
             // 
@@ -64,34 +69,6 @@
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(0, 13);
             this.label2.TabIndex = 1;
-            // 
-            // grilla_ABMAsiento
-            // 
-            this.grilla_ABMAsiento.AllowUserToAddRows = false;
-            this.grilla_ABMAsiento.AllowUserToDeleteRows = false;
-            this.grilla_ABMAsiento.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.grilla_ABMAsiento.Location = new System.Drawing.Point(32, 178);
-            this.grilla_ABMAsiento.Name = "grilla_ABMAsiento";
-            this.grilla_ABMAsiento.Pp_FormatoGrid = "";
-            this.grilla_ABMAsiento.Pp_NombreTabla = "Asientos";
-            this.grilla_ABMAsiento.ReadOnly = true;
-            this.grilla_ABMAsiento.Size = new System.Drawing.Size(590, 150);
-            this.grilla_ABMAsiento.TabIndex = 2;
-            // 
-            // cmb_NumeroAvion
-            // 
-            this.cmb_NumeroAvion.FormattingEnabled = true;
-            this.cmb_NumeroAvion.Location = new System.Drawing.Point(228, 95);
-            this.cmb_NumeroAvion.Name = "cmb_NumeroAvion";
-            this.cmb_NumeroAvion.Pp_CampoAceptaNull = true;
-            this.cmb_NumeroAvion.Pp_EsPk = false;
-            this.cmb_NumeroAvion.Pp_MensajeError = "Seleccione un número de avión";
-            this.cmb_NumeroAvion.Pp_NombreCampo = "numeroPorModelo";
-            this.cmb_NumeroAvion.Pp_NombreCampoInsert = "numeroPorModelo";
-            this.cmb_NumeroAvion.Pp_NombreTabla = "Avion";
-            this.cmb_NumeroAvion.Pp_PkTabla = "numeroPorModelo";
-            this.cmb_NumeroAvion.Size = new System.Drawing.Size(195, 21);
-            this.cmb_NumeroAvion.TabIndex = 3;
             // 
             // label3
             // 
@@ -119,83 +96,6 @@
             this.label5.Size = new System.Drawing.Size(42, 13);
             this.label5.TabIndex = 6;
             this.label5.Text = "Modelo";
-            // 
-            // cmb_Modelo
-            // 
-            this.cmb_Modelo.FormattingEnabled = true;
-            this.cmb_Modelo.Location = new System.Drawing.Point(228, 62);
-            this.cmb_Modelo.Name = "cmb_Modelo";
-            this.cmb_Modelo.Pp_CampoAceptaNull = true;
-            this.cmb_Modelo.Pp_EsPk = false;
-            this.cmb_Modelo.Pp_MensajeError = "Seleccione un modelo de avión";
-            this.cmb_Modelo.Pp_NombreCampo = "nombre";
-            this.cmb_Modelo.Pp_NombreCampoInsert = "idModelo";
-            this.cmb_Modelo.Pp_NombreTabla = "Modelo";
-            this.cmb_Modelo.Pp_PkTabla = "idModelo";
-            this.cmb_Modelo.Size = new System.Drawing.Size(195, 21);
-            this.cmb_Modelo.TabIndex = 7;
-            // 
-            // btn_buscar
-            // 
-            this.btn_buscar.Location = new System.Drawing.Point(648, 178);
-            this.btn_buscar.Name = "btn_buscar";
-            this.btn_buscar.Pp_Presionado = false;
-            this.btn_buscar.Size = new System.Drawing.Size(75, 23);
-            this.btn_buscar.TabIndex = 8;
-            this.btn_buscar.Text = "Buscar ";
-            this.btn_buscar.UseVisualStyleBackColor = true;
-            this.btn_buscar.Click += new System.EventHandler(this.btn_buscar_Click);
-            // 
-            // btn_registrar
-            // 
-            this.btn_registrar.Location = new System.Drawing.Point(648, 218);
-            this.btn_registrar.Name = "btn_registrar";
-            this.btn_registrar.Pp_Presionado = false;
-            this.btn_registrar.Size = new System.Drawing.Size(75, 23);
-            this.btn_registrar.TabIndex = 9;
-            this.btn_registrar.Text = "Registrar";
-            this.btn_registrar.UseVisualStyleBackColor = true;
-            // 
-            // btn_modificar
-            // 
-            this.btn_modificar.Location = new System.Drawing.Point(648, 266);
-            this.btn_modificar.Name = "btn_modificar";
-            this.btn_modificar.Pp_Presionado = false;
-            this.btn_modificar.Size = new System.Drawing.Size(75, 23);
-            this.btn_modificar.TabIndex = 10;
-            this.btn_modificar.Text = "Modificar";
-            this.btn_modificar.UseVisualStyleBackColor = true;
-            // 
-            // btn_eliminar
-            // 
-            this.btn_eliminar.Location = new System.Drawing.Point(648, 305);
-            this.btn_eliminar.Name = "btn_eliminar";
-            this.btn_eliminar.Pp_Presionado = false;
-            this.btn_eliminar.Size = new System.Drawing.Size(75, 23);
-            this.btn_eliminar.TabIndex = 11;
-            this.btn_eliminar.Text = "Eliminar";
-            this.btn_eliminar.UseVisualStyleBackColor = true;
-            // 
-            // btn_cerrar
-            // 
-            this.btn_cerrar.Location = new System.Drawing.Point(32, 379);
-            this.btn_cerrar.Name = "btn_cerrar";
-            this.btn_cerrar.Pp_Presionado = false;
-            this.btn_cerrar.Size = new System.Drawing.Size(75, 23);
-            this.btn_cerrar.TabIndex = 12;
-            this.btn_cerrar.Text = "Cerrar";
-            this.btn_cerrar.UseVisualStyleBackColor = true;
-            this.btn_cerrar.Click += new System.EventHandler(this.btn_cerrar_Click);
-            // 
-            // btn_limpiar
-            // 
-            this.btn_limpiar.Location = new System.Drawing.Point(125, 379);
-            this.btn_limpiar.Name = "btn_limpiar";
-            this.btn_limpiar.Pp_Presionado = false;
-            this.btn_limpiar.Size = new System.Drawing.Size(75, 23);
-            this.btn_limpiar.TabIndex = 13;
-            this.btn_limpiar.Text = "Limpiar";
-            this.btn_limpiar.UseVisualStyleBackColor = true;
             // 
             // chk_asientos
             // 
@@ -230,6 +130,153 @@
             this.cmb_Clase.Pp_PkTabla = "idTipo";
             this.cmb_Clase.Size = new System.Drawing.Size(195, 21);
             this.cmb_Clase.TabIndex = 16;
+            // 
+            // btn_limpiar
+            // 
+            this.btn_limpiar.Location = new System.Drawing.Point(125, 379);
+            this.btn_limpiar.Name = "btn_limpiar";
+            this.btn_limpiar.Pp_Presionado = false;
+            this.btn_limpiar.Size = new System.Drawing.Size(75, 23);
+            this.btn_limpiar.TabIndex = 13;
+            this.btn_limpiar.Text = "Limpiar";
+            this.btn_limpiar.UseVisualStyleBackColor = true;
+            this.btn_limpiar.Click += new System.EventHandler(this.btn_limpiar_Click);
+            // 
+            // btn_cerrar
+            // 
+            this.btn_cerrar.Location = new System.Drawing.Point(32, 379);
+            this.btn_cerrar.Name = "btn_cerrar";
+            this.btn_cerrar.Pp_Presionado = false;
+            this.btn_cerrar.Size = new System.Drawing.Size(75, 23);
+            this.btn_cerrar.TabIndex = 12;
+            this.btn_cerrar.Text = "Cerrar";
+            this.btn_cerrar.UseVisualStyleBackColor = true;
+            this.btn_cerrar.Click += new System.EventHandler(this.btn_cerrar_Click);
+            // 
+            // btn_eliminar
+            // 
+            this.btn_eliminar.Enabled = false;
+            this.btn_eliminar.Location = new System.Drawing.Point(648, 305);
+            this.btn_eliminar.Name = "btn_eliminar";
+            this.btn_eliminar.Pp_Presionado = false;
+            this.btn_eliminar.Size = new System.Drawing.Size(75, 23);
+            this.btn_eliminar.TabIndex = 11;
+            this.btn_eliminar.Text = "Eliminar";
+            this.btn_eliminar.UseVisualStyleBackColor = true;
+            // 
+            // btn_modificar
+            // 
+            this.btn_modificar.Enabled = false;
+            this.btn_modificar.Location = new System.Drawing.Point(648, 266);
+            this.btn_modificar.Name = "btn_modificar";
+            this.btn_modificar.Pp_Presionado = false;
+            this.btn_modificar.Size = new System.Drawing.Size(75, 23);
+            this.btn_modificar.TabIndex = 10;
+            this.btn_modificar.Text = "Modificar";
+            this.btn_modificar.UseVisualStyleBackColor = true;
+            // 
+            // btn_registrar
+            // 
+            this.btn_registrar.Location = new System.Drawing.Point(648, 218);
+            this.btn_registrar.Name = "btn_registrar";
+            this.btn_registrar.Pp_Presionado = false;
+            this.btn_registrar.Size = new System.Drawing.Size(75, 23);
+            this.btn_registrar.TabIndex = 9;
+            this.btn_registrar.Text = "Registrar";
+            this.btn_registrar.UseVisualStyleBackColor = true;
+            // 
+            // btn_buscar
+            // 
+            this.btn_buscar.Location = new System.Drawing.Point(648, 178);
+            this.btn_buscar.Name = "btn_buscar";
+            this.btn_buscar.Pp_Presionado = false;
+            this.btn_buscar.Size = new System.Drawing.Size(75, 23);
+            this.btn_buscar.TabIndex = 8;
+            this.btn_buscar.Text = "Buscar ";
+            this.btn_buscar.UseVisualStyleBackColor = true;
+            this.btn_buscar.Click += new System.EventHandler(this.btn_buscar_Click);
+            // 
+            // cmb_Modelo
+            // 
+            this.cmb_Modelo.FormattingEnabled = true;
+            this.cmb_Modelo.Location = new System.Drawing.Point(228, 62);
+            this.cmb_Modelo.Name = "cmb_Modelo";
+            this.cmb_Modelo.Pp_CampoAceptaNull = true;
+            this.cmb_Modelo.Pp_EsPk = false;
+            this.cmb_Modelo.Pp_MensajeError = "Seleccione un modelo de avión";
+            this.cmb_Modelo.Pp_NombreCampo = "nombre";
+            this.cmb_Modelo.Pp_NombreCampoInsert = "idModelo";
+            this.cmb_Modelo.Pp_NombreTabla = "Modelo";
+            this.cmb_Modelo.Pp_PkTabla = "idModelo";
+            this.cmb_Modelo.Size = new System.Drawing.Size(195, 21);
+            this.cmb_Modelo.TabIndex = 7;
+            this.cmb_Modelo.SelectionChangeCommitted += new System.EventHandler(this.cmb_Modelo_SelectionChangeCommitted);
+            // 
+            // cmb_NumeroAvion
+            // 
+            this.cmb_NumeroAvion.FormattingEnabled = true;
+            this.cmb_NumeroAvion.Location = new System.Drawing.Point(228, 95);
+            this.cmb_NumeroAvion.Name = "cmb_NumeroAvion";
+            this.cmb_NumeroAvion.Pp_CampoAceptaNull = true;
+            this.cmb_NumeroAvion.Pp_EsPk = false;
+            this.cmb_NumeroAvion.Pp_MensajeError = "Seleccione un número de avión";
+            this.cmb_NumeroAvion.Pp_NombreCampo = "numeroPorModelo";
+            this.cmb_NumeroAvion.Pp_NombreCampoInsert = "numeroPorModelo";
+            this.cmb_NumeroAvion.Pp_NombreTabla = "Avion";
+            this.cmb_NumeroAvion.Pp_PkTabla = "numeroPorModelo";
+            this.cmb_NumeroAvion.Size = new System.Drawing.Size(195, 21);
+            this.cmb_NumeroAvion.TabIndex = 3;
+            this.cmb_NumeroAvion.SelectionChangeCommitted += new System.EventHandler(this.cmb_NumeroAvion_SelectionChangeCommitted);
+            // 
+            // grilla_ABMAsiento
+            // 
+            this.grilla_ABMAsiento.AllowUserToAddRows = false;
+            this.grilla_ABMAsiento.AllowUserToDeleteRows = false;
+            this.grilla_ABMAsiento.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grilla_ABMAsiento.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5});
+            this.grilla_ABMAsiento.Location = new System.Drawing.Point(32, 178);
+            this.grilla_ABMAsiento.Name = "grilla_ABMAsiento";
+            this.grilla_ABMAsiento.Pp_FormatoGrid = "";
+            this.grilla_ABMAsiento.Pp_NombreTabla = "Asientos";
+            this.grilla_ABMAsiento.ReadOnly = true;
+            this.grilla_ABMAsiento.Size = new System.Drawing.Size(590, 150);
+            this.grilla_ABMAsiento.TabIndex = 2;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Numero de asiento";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Modelo del Avión";
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Número del avión";
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Clase";
+            this.Column4.Name = "Column4";
+            this.Column4.ReadOnly = true;
+            this.Column4.Width = 110;
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "Estado";
+            this.Column5.Name = "Column5";
+            this.Column5.ReadOnly = true;
             // 
             // Frm_ABMAsientos
             // 
@@ -283,5 +330,10 @@
         private System.Windows.Forms.CheckBox chk_asientos;
         private System.Windows.Forms.Label label6;
         private Clase.ComboBox_Aerolinea cmb_Clase;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
     }
 }
--- TrabajoPrácticoPAV/Formularios/Asientos/Frm_ABMAsientos.cs
+++ TrabajoPrácticoPAV/Formularios/Asientos/Frm_ABMAsientos.cs
@@ -8,6 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using TrabajoPrácticoPAV.NE_Usuarios;
+using TrabajoPrácticoPAV.Clase;
+using TrabajoPrácticoPAV.Backend;
 
 namespace TrabajoPrácticoPAV.Formularios.Asientos
 {
@@ -25,50 +27,92 @@ namespace TrabajoPrácticoPAV.Formularios.Asientos
 
         private void Frm_ABMAsientos_Load(object sender, EventArgs e)
         {
-            string condicionAvion = "";
-            string condicionClase = "";
             cmb_Modelo.CargarCombo();
-            cmb_NumeroAvion.CargarComboJoin(condicionAvion);
-            cmb_Clase.CargarComboJoin(condicionClase);
-
-        
         }
 
         private void btn_buscar_Click(object sender, EventArgs e)
         {
             NE_Asiento asiento = new NE_Asiento();
+            Tratamientos_Especiales tratamiento = new Tratamientos_Especiales();
 
-            if (chk_asientos.Checked == false && cmb_NumeroAvion.SelectedIndex == -1 && cmb_Modelo.SelectedIndex == -1)
-            {
-                MessageBox.Show("Debe seleccionar alguna opción");
-            }
-            if (chk_asientos.Checked == true)
-            {
-                DataTable tabla = new DataTable();
-                tabla = asiento.RecuperarTodos();
-                CargarGrilla_asientos(tabla);
-                return;
-            }
-            if (chk_asientos.Checked == false
-                && cmb_NumeroAvion.SelectedIndex != -1
-                && cmb_Modelo.SelectedIndex != -1)
-            {
-                CargarGrilla_asientos(asiento.Recuperar_Mixto((cmb_NumeroAvion.SelectedValue.ToString()), cmb_Modelo.SelectedValue.ToString()));
-                return;
-            }
-            if (cmb_Modelo.SelectedIndex != -1)
-            {
-                CargarGrilla_asientos(asiento.RecuprarXmodelo(cmb_Modelo.SelectedValue.ToString()));
-                return;
-            }
-            if (cmb_NumeroAvion.SelectedIndex != -1)
-            {
-                CargarGrilla_asientos(asiento.RecuperarXavion(cmb_NumeroAvion.SelectedValue.ToString()));
-            }
+           string sql = tratamiento.ConstructorSelect(this.Controls, "");
+            CargarGrilla_asientos(sql);
+            //if (chk_asientos.Checked == false && cmb_NumeroAvion.SelectedIndex == -1 && cmb_Modelo.SelectedIndex == -1)
+            //{
+            //    MessageBox.Show("Debe seleccionar alguna opción");
+            //}
+            //if (chk_asientos.Checked == true)
+            //{
+            //    DataTable tabla = new DataTable();
+            //    tabla = asiento.BuscarTodos();
+            //    CargarGrilla_asientos(tabla);
+            //    return;
+            //}
+            //if (chk_asientos.Checked == false
+            //    && cmb_NumeroAvion.SelectedIndex != -1
+            //    && cmb_Modelo.SelectedIndex != -1)
+            //{
+            //    CargarGrilla_asientos(asiento.Recuperar_Mixto((cmb_NumeroAvion.SelectedValue.ToString()), cmb_Modelo.SelectedValue.ToString()));
+            //    return;
+            //}
+            //if (cmb_Modelo.SelectedIndex != -1)
+            //{
+            //    CargarGrilla_asientos(asiento.RecuprarXmodelo(cmb_Modelo.SelectedValue.ToString()));
+            //    return;
+            //}
+            //if (cmb_NumeroAvion.SelectedIndex != -1)
+            //{
+            //    CargarGrilla_asientos(asiento.RecuperarXavion(cmb_NumeroAvion.SelectedValue.ToString()));
+            //}
         }
-        private void CargarGrilla_asientos(DataTable tabla)
+        private void CargarGrilla_asientos(string sql)
         {
+            Conexion_DB _BD = new Conexion_DB();
+            DataTable tabla = _BD.EjecutarSelect(sql);
 
+            grilla_ABMAsiento.Rows.Clear();
+
+            for (int i = 0; i < tabla.Rows.Count; i++)
+            {
+                grilla_ABMAsiento.Rows.Add();
+                grilla_ABMAsiento.Rows[i].Cells[0].Value = tabla.Rows[i]["numeroAsiento"].ToString();
+                //grilla_ABMAsiento.Rows[i].Cells[1].Value = tabla.Rows[i]["n_modelo"].ToString();
+                grilla_ABMAsiento.Rows[i].Cells[1].Value = tabla.Rows[i]["idModelo"].ToString();
+                grilla_ABMAsiento.Rows[i].Cells[2].Value = tabla.Rows[i]["numeroPorModelo"].ToString();
+                //grilla_ABMAsiento.Rows[i].Cells[3].Value = tabla.Rows[i]["n_Asiento"].ToString();
+                grilla_ABMAsiento.Rows[i].Cells[3].Value = tabla.Rows[i]["tipoAsiento"].ToString();
+                grilla_ABMAsiento.Rows[i].Cells[4].Value = tabla.Rows[i]["estado"].ToString();
+            }
+            if (tabla.Rows.Count == 0)
+            {
+                MessageBox.Show("No se encontraron Asientos para ese filtro.");
+            }
+
+        }
+
+        private void cmb_Modelo_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+            string CondicionAvion = @" JOIN Modelo ON Modelo.idModelo " +
+                       @"= Avion.idModelo WHERE Modelo.idModelo = " + cmb_Modelo.SelectedValue;
+            cmb_NumeroAvion.CargarComboJoin(CondicionAvion);
+            chk_asientos.Checked = false;
+        }
+
+        private void cmb_NumeroAvion_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+            string condicionClase = @"JOIN Asientos ON Tipo_Asiento.idTipo" +
+                              @"= Asientos.tipoAsiento WHERE Asientos.numeroPorModelo = " + cmb_NumeroAvion.SelectedValue;
+            cmb_Clase.CargarComboJoin(condicionClase);
+            chk_asientos.Checked = false;
+        }
+
+        private void btn_limpiar_Click(object sender, EventArgs e)
+        {
+            cmb_Modelo.SelectedIndex = -1;
+            cmb_NumeroAvion.SelectedIndex = -1;
+            cmb_Clase.SelectedIndex = -1;
+            chk_asientos.Checked = false;
+            grilla_ABMAsiento.Rows.Clear();
         }
     }
 }
