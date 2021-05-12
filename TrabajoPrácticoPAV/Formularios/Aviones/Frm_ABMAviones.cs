87759e0bf4346edb9611908c2c6dccd1abb51659
unknown <sofia.velazco19.98@gmail.com>
2021-05-12 20:03:33 -0300

    botones avion

--- TrabajoPrácticoPAV/Formularios/Aviones/Frm_ABMAviones.cs
+++ TrabajoPrácticoPAV/Formularios/Aviones/Frm_ABMAviones.cs
@@ -28,7 +28,6 @@ namespace TrabajoPrácticoPAV.Formularios
             Estilo.FormatearEstilo(this.Controls);
         }
 
-
         private void CargarGrilla(DataTable tabla)
         {
             grid_aviones.Rows.Clear();
@@ -41,9 +40,6 @@ namespace TrabajoPrácticoPAV.Formularios
 
             }
         }
-
-
-
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
             int current = grid_aviones.CurrentRow.Index;
@@ -60,52 +56,13 @@ namespace TrabajoPrácticoPAV.Formularios
                 MessageBox.Show("Avion eliminado, Actualize la tabla");
                 return;
             }
-
-
-
         }
-
-        private void panel1_Paint(object sender, PaintEventArgs e)
-        {
-
-        }
-
-        private void btn_alta_Click(object sender, EventArgs e)
-        {
-            Frm_AltaAviones alta = new Frm_AltaAviones();
-            alta.ShowDialog();
-
-
-        }
-
         private void Frm_ABMAviones_Load(object sender, EventArgs e)
         {
             cmb_Modelo.CargarCombo();
         }
 
-
-        private void btn_actualizar_Click(object sender, EventArgs e)
-        {
-
-            NE_Avion aviones = new NE_Avion();
-            DataTable tabla = new DataTable();
-            tabla = aviones.RecuperarTodos();
-            CargarGrilla(tabla);
-
-        }
-
-        private void textBox_Aerolinea1_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-
-        private void cmb_Modelo_SelectedIndexChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void btn_buscar_Click_1(object sender, EventArgs e)
+        private void btn_buscar_Click(object sender, EventArgs e)
         {
             int modelo_up = int.Parse(cmb_Modelo.SelectedIndex.ToString()) + 1;
             NE_Avion avion = new NE_Avion();
@@ -137,61 +94,15 @@ namespace TrabajoPrácticoPAV.Formularios
                 return;
             }
 
-
         }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button3_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void picBox_buscar_Click(object sender, EventArgs e)
-        {
-            int modelo_up = int.Parse(cmb_Modelo.SelectedIndex.ToString()) + 1;
-            NE_Avion avion = new NE_Avion();
-
-            if (txt_numero.Text.Length > 4)
-            {
-                MessageBox.Show("El numero de avion debe tener 4 numeros");
-            }
-            if (txt_numero.Text == string.Empty && cmb_Modelo.SelectedIndex == -1)
-            {
-                MessageBox.Show("Seleccionar alguna opcion o ingresar numero de modelo");
-            }
-
-            if (cmb_Modelo.SelectedIndex != -1 && txt_numero.Text != "")
-            {
-                CargarGrilla(avion.Recuperar_Mixto(txt_numero.Text, modelo_up));
-                return;
-            }
-
-            if (cmb_Modelo.SelectedIndex != -1)
-            {
-                CargarGrilla(avion.Recuperar_x_modelo(modelo_up));
-                return;
-            }
-
-            if (txt_numero.Text != "")
-            {
-                CargarGrilla(avion.Recuperar_x_numero(txt_numero.Text));
-                return;
-            }
-        }
-
-        private void picBox_actualizar_Click(object sender, EventArgs e)
+        private void btn_actualizar_Click_1(object sender, EventArgs e)
         {
             NE_Avion aviones = new NE_Avion();
             DataTable tabla = new DataTable();
             tabla = aviones.RecuperarTodos();
             CargarGrilla(tabla);
         }
-
-        private void picBox_agregar_Click(object sender, EventArgs e)
+        private void btn_agregar_Click(object sender, EventArgs e)
         {
             Frm_AltaAviones alta = new Frm_AltaAviones();
             alta.ShowDialog();
