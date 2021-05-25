20517e1f71d0b548f9e1b8dbeb1a122b0876adb2
unknown <sofia.velazco19.98@gmail.com>
2021-05-25 15:03:13 -0300

    Al buscar la reserva carga la grilla y los datos del pasajero. Falta la programacion de los botones generar factura y eliminar

--- TrabajoPrácticoPAV/NE_Usuarios/NE_Facturacion.cs
+++ TrabajoPrácticoPAV/NE_Usuarios/NE_Facturacion.cs
@@ -19,5 +19,13 @@ namespace TrabajoPrácticoPAV.NE_Usuarios
                                     + " AND R.tipoDocTitular = P.tipoDoc WHERE numeroDeReserva = " + reserva);
         }
         
+        public DataTable RecuperarVueloxReserva(string reserva)
+        {
+            return _BD.EjecutarSelect(@"SELECT V.idVuelo, V.codigoAeropuertoDestino, V.codigoAeropuertoSalida"
+                                      + " FROM Reserva R JOIN Viaje_X_Tramo VT ON R.numeroDeViaje= VT.numeroDeViaje "
+                                      + " JOIN Vuelo V ON V.codigoAeropuertoDestino = VT.codigoAeropuertoDestino"
+                                      + " AND V.codigoAeropuertoSalida = VT.codigoAeropuertoSalida " 
+                                      + " WHERE R.numeroDeReserva = " + reserva);
+        }
     }
 }
