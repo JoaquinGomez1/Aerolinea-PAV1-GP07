29f017a499eab4eacb3bc7f62dffef4f27fbac27
Mora de Bianchetti <mdebianchetti81@gmail.com>
2021-06-07 18:49:17 -0300

    Reporte falta terminar

---
+++ TrabajoPrácticoPAV/Formularios/Listados/Aeropuertos/Frm_ReporteAeroPais.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TrabajoPrácticoPAV.Formularios.Listados.Aeropuertos
+{
+    public partial class Frm_ReporteAeroPais : Form
+    {
+        public Frm_ReporteAeroPais()
+        {
+            InitializeComponent();
+        }
+    }
+}
