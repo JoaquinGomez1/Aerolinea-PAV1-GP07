72d67c16ebe5ea98cd87cd84c1c8f23c6b16d9f4
tefimontalvan <tefi386@gmail.com>
2021-06-23 11:43:37 -0300

    filtro en estadistica pasajeros por clase

--- TrabajoPrácticoPAV/Formularios/Estadisticas/PasajerosPorClase/Frm_PasajerosPorClase.cs
+++ TrabajoPrácticoPAV/Formularios/Estadisticas/PasajerosPorClase/Frm_PasajerosPorClase.cs
@@ -26,18 +26,8 @@ namespace TrabajoPrácticoPAV.Formularios.Estadisticas.PasajerosPorViaje
 
         private void Frm_PasajerosPorViaje_Load(object sender, EventArgs e)
         {
-            DataTable tabla = determineDataSet();
+            
             this.rv_pasajerosPorViaje.RefreshReport();
-
-
-            if (tabla.Rows.Count != 0)
-                CargarEstadisticaPasajerosPorClase(tabla);
-            else
-            {
-                rv_pasajerosPorViaje.LocalReport.DataSources.Clear();
-                rv_pasajerosPorViaje.RefreshReport();
-                MessageBox.Show("No se encontraron resultados para la búsqueda");
-            }
         }
 
         public void CargarEstadisticaPasajerosPorClase(DataTable tabla)
@@ -57,6 +47,38 @@ namespace TrabajoPrácticoPAV.Formularios.Estadisticas.PasajerosPorViaje
         private void btn_cerrar_Click(object sender, EventArgs e)
         {
             this.Close();
+        }
+
+        private void btn_calcular_Click(object sender, EventArgs e)
+        {
+            if (rbu_todos.Checked == true)
+            {
+                DataTable tabla = determineDataSet();
+                if (tabla.Rows.Count != 0)
+                    CargarEstadisticaPasajerosPorClase(tabla);
+                else
+                {
+                    rv_pasajerosPorViaje.LocalReport.DataSources.Clear();
+                    rv_pasajerosPorViaje.RefreshReport();
+                    MessageBox.Show("No se encontraron resultados para la búsqueda");
+                }
+            }
+            else if (rbu_año.Checked == true)
+            {
+                if (mtxt_año.Text != null)
+                {
+                    int añoSeleccionado = Int32.Parse(mtxt_año.Text);
+                    DataTable tabla2 = pasajero.RecuperarPorAño(añoSeleccionado);
+                    if (tabla2.Rows.Count != 0)
+                        CargarEstadisticaPasajerosPorClase(tabla2);
+                    else
+                    {
+                        rv_pasajerosPorViaje.LocalReport.DataSources.Clear();
+                        rv_pasajerosPorViaje.RefreshReport();
+                        MessageBox.Show("No se encontraron resultados para la búsqueda");
+                    }
+                }
+            }
         }
     }
 }
